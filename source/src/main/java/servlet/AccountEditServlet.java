f24c022b47b8bd5e760c4c9b7c413774e0e72e13
plusdojo2026-hieda <hieda-arisa-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 16:29:33 +0900

    エラーが出ていたサーブレットの箇所修正しました。

--- source/src/main/java/servlet/AccountEditServlet.java
+++ source/src/main/java/servlet/AccountEditServlet.java
@@ -14,23 +14,32 @@ import model.Account;
 
 @WebServlet("/account/edit")
 public class AccountEditServlet extends HttpServlet {
-
+	private static final long serialVersionUID = 1L;
+	
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
+    	
+    	request.setCharacterEncoding("UTF-8");
+    	
+    	// 1.編集モーダル内のhiddenフィールドから従業員IDを取得して数値に変換
+    	int id = Integer.parseInt(request.getParameter("id"));
+    	
+    	// 2.編集モーダル内の入力項目を取得
+    	String newName = request.getParameter("newName");
+    	int newPermissionsId = Integer.parseInt(request.getParameter("newPermissionsId"));
 
-        Account p = new Account();
-        p.setJanCode(request.getParameter("janCode"));
-        p.setProductName(request.getParameter("productName"));
-        p.setBaseProductId(request.getParameter("baseProductId"));
-        p.setCaseQuantity(Integer.parseInt(request.getParameter("caseQuantity")));
-        p.setPhotoPath(request.getParameter("photoPath"));
-        p.setDurationDays(Integer.parseInt(request.getParameter("durationDays")));
-
+    	// 3.従業員(Account)オブジェクトに値をセット
+        Account account = new Account();
+        account.setId(id);
+        account.setName(newName);
+        account.setPermissionsId(newPermissionsId);
+        
+        // 4.DAOを呼び出してデータベースを更新(UPDATE)
         AccountDAO dao = new AccountDAO();
-        dao.update(p);
+        dao.update(account);
 
-        response.sendRedirect("/c4/admin");
+        // 5.処理完了後、Accountサーブレットにリダイレクト
+        response.sendRedirect(request.getContextPath() + "/account");
     }
 }
-
