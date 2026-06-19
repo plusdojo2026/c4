7fbbc497f7e142ba90dc3ecb9f3a46d5df4ace30
plusdojo2026-fujimori <fujimori-daiki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 16:34:30 +0900

    ルートサーブレットのURLパターンを修正しました。

--- source/src/main/java/servlet/RootServlet.java
+++ source/src/main/java/servlet/RootServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("c4")
+@WebServlet("")
 public class RootServlet extends HttpServlet {
 
 
