dfca0bd2d8f04fe429146f2c81622e5ad02a1fae
plusdojo2026-majima <majima-ryota-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-29 14:13:48 +0900

    Update product.js

--- source/src/main/webapp/js/product.js
+++ source/src/main/webapp/js/product.js
@@ -692,7 +692,7 @@ editImageInput?.addEventListener("change", function () {
         };
         reader.readAsDataURL(file);
 
-        // ★ 修正ポイント：fileName → file.name
+        
         document.getElementById("edit-file-name").textContent = file.name;
     }
 });
